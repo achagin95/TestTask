1c442335356850ba0c95dff41357e6f3d8cc8535
Andrey Chagin <a.chagin95@mail.ru>
2021-05-11 17:01:17 +0600

    1105

--- data/sqlScript.sql
+++ data/sqlScript.sql
@@ -5,11 +5,28 @@ USE `testTaskSmartForce`;
 CREATE TABLE houses (
 id INT (11) NOT NULL auto_increment,
 product varchar(45) NOT NULL,
-address varchar(45) not null,
+addressStreet varchar(45) not null,
+addressCity varchar(45) not null,
+addressState varchar(45) not null,
+addressZip varchar(45) not null,
+addressCoorLatitude varchar(45) not null,
+addressCoorLongitude varchar(45) not null,
+builder varchar(45) not null,
+price int(11) not null,
+phone varchar(45) not null,
+square int(11) not null,
+hasBasement boolean,
+description varchar(45) not null,
+garage int(11) not null,
+bedrooms int(11) not null,
+type varchar(45) not null,
+/*amenities boolean,
+images*/
 PRIMARY KEY (id),
-unique key address (address)
+unique key addressStreet (addressStreet)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
-
+/*учесть что тайп подсветился, возможно придется заменить
+еще поправить уникальный ключ позже*/
 create user "testUser"@"%" identified by "test";
 select User,Host from mysql.user;
 grant all privileges on testTaskSmartForce.* to 'testUser'@'%';
@@ -19,4 +36,4 @@ flush privileges;
 
 show tables;
 select * from houses;
-insert into houses ( product, address) values ("dom3", "24 street");
+insert into houses ( product, address) values ("dom3", "20 street");
